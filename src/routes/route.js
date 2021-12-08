a83f053d2c7758f2787e0750b3a3b9f0e29e487f
sabihak89 <87858386+sabihak89@users.noreply.github.com>
2021-12-08 12:41:30 +0530

    chore : removed extra routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,15 +12,5 @@ router.get('/collegeDetails', collegeController.collegeDetails)
 // Intern routes
 router.post('/interns', internController.createIntern)
 
-// Netcore webhook test routes
-router.post('/deliveryWebhook', function(req, res) {
-    console.log(req)
-    res.status(200).send()
-})
-
-router.post('/incomingWebhook', function(req, res) {
-    console.log(req)
-    res.status(200).send()
-})
 
 module.exports = router;
