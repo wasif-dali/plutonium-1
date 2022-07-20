185783a94a8beb7ec78e65daddcaf966718d40b1
Sabiha Khan <sabiha.khan@functionup.org>
2022-07-20 11:59:12 +0530

    chore: rename author collection

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -9,4 +9,4 @@ const authorSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("myAuthor", authorSchema);
+module.exports = mongoose.model("myAuthorOther", authorSchema);
